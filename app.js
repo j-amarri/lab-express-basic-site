e6d4f0a5276f134c5a8cf8ebdb9ac64c3208d24f
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-07-21 18:30:29 +0100

    Handlebars added

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+hbs.registerPartials(__dirname + '/views/partials');
+
 app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/views/about.html');
+  response.render('about');
+  //response.sendFile(__dirname + '/views/about.html');
 });
 app.get('/home', (request, response) => {
-  response.sendFile(__dirname + '/views/home.html');
+  response.render('home');
+  //response.sendFile(__dirname + '/views/home.html');
 });
 app.get('/songs', (request, response) => {
-  response.sendFile(__dirname + '/views/songs.html');
+  response.render('songs');
+  //response.sendFile(__dirname + '/views/songs.html');
 });
 app.get('*', (request, response) => {
-  response.send('Page under construction');
+  response.render();
+  //response.send('Page under construction');
 });
 
 app.listen(3000);
